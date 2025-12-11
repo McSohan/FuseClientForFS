9062e41f75d5a6b540f8d0e1b7d881be60d22148
user <user@Debian.myguest.virtualbox.org>
2025-12-10 19:21:18 -0800

    Impl stat with chrono dep

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,9 @@ use std::path::Path;
 
 use fuse_client_for_fs::transport::unix_socket::FuseListener;
 use fuse_client_for_fs::protocol::FuseProtocol;
+use fuse_client_for_fs::shell::commands::FuseShell;
 
+/**
 fn main() -> std::io::Result<()> {
     let socket_path = "/tmp/fuse.sock";
     let path = Path::new(socket_path);
@@ -43,6 +45,27 @@ fn main() -> std::io::Result<()> {
 
     proto.release(entry.nodeid, open_out.fh)?;
 
+
+    Ok(())
+}
+**/
+
+fn main() -> std::io::Result<()> {
+    let socket_path = "/tmp/fuse.sock";
+
+    if Path::new(socket_path).exists() {
+        fs::remove_file(socket_path)?;
+    }
+
+    let transport = FuseListener::bind(socket_path)?.accept()?;
+    let mut proto = FuseProtocol::new(transport);
+
+    let init = proto.send_init()?;
+    println!("[Debug] FUSE Initialized: major={} minor={}, congestion_threshold={}", init.major, init.minor, init.congestion_threshold);
+    println!("FUSE Init complete, entering shell…");
+
+    let mut sh = FuseShell::new(proto)?;
+    sh.run()?;
 
     Ok(())
 }
--- src/shell/commands.rs
+++ src/shell/commands.rs
@@ -69,12 +69,77 @@ impl FuseShell {
         Ok(())
     }
 
-    fn cmd_stat(&mut self, name: &str) -> io::Result<()> {
-        let inode = self.lookup_name(name)?;
-        let attr = self.proto.getattr(inode)?;
+    // fn cmd_stat(&mut self, name: &str) -> io::Result<()> {
+    //     let inode = self.lookup_name(name)?;
+    //     let attr = self.proto.getattr(inode)?;
 
-        println!("inode={} size={} mode={:o}",
-            attr.attr.ino, attr.attr.size, attr.attr.mode);
+    //     println!("inode={} size={} mode={:o}",
+    //         attr.attr.ino, attr.attr.size, attr.attr.mode);
+
+    //     Ok(())
+    // }
+
+    fn cmd_stat(&mut self, name: &str) -> io::Result<()> {
+        use std::time::{UNIX_EPOCH, Duration};
+
+        let inode = self.lookup_name(name)?;
+        let attr_out = self.proto.getattr(inode)?;
+        let attr = attr_out.attr;
+
+        // 1. Decode file type
+        let mode = attr.mode;
+        let ftype = match mode & libc::S_IFMT {
+            libc::S_IFREG => "regular file",
+            libc::S_IFDIR => "directory",
+            libc::S_IFLNK => "symbolic link",
+            libc::S_IFCHR => "character device",
+            libc::S_IFBLK => "block device",
+            libc::S_IFIFO => "FIFO/pipe",
+            libc::S_IFSOCK => "socket",
+            _ => "unknown",
+        };
+
+        // 2. Permissions, symbolic rwxr-xr-x
+        fn mode_to_string(mode: u32) -> String {
+            let mut s = String::new();
+            let perms = [
+                (libc::S_IRUSR, 'r'), (libc::S_IWUSR, 'w'), (libc::S_IXUSR, 'x'),
+                (libc::S_IRGRP, 'r'), (libc::S_IWGRP, 'w'), (libc::S_IXGRP, 'x'),
+                (libc::S_IROTH, 'r'), (libc::S_IWOTH, 'w'), (libc::S_IXOTH, 'x'),
+            ];
+            for &(bit, ch) in &perms {
+                s.push(if mode & bit != 0 { ch } else { '-' });
+            }
+            s
+        }
+
+        let perm_string = mode_to_string(mode);
+
+        // Helper for timestamps
+        fn fmt_time(sec: u64, nsec: u32) -> String {
+            let ts = UNIX_EPOCH + Duration::new(sec, nsec);
+            let datetime: chrono::DateTime<chrono::Local> = ts.into();
+            datetime.format("%Y-%m-%d %H:%M:%S.%f").to_string()
+        }
+
+        let atime = fmt_time(attr.atime, attr.atimensec);
+        let mtime = fmt_time(attr.mtime, attr.mtimensec);
+        let ctime = fmt_time(attr.ctime, attr.ctimensec);
+
+        println!("  File: {}", name);
+        println!("  Size: {:<10} Blocks: {:<10} IO Block: {}", attr.size, attr.blocks, attr.blksize);
+        println!("Device: {} Inode: {}  Links: {}", attr.rdev, attr.ino, attr.nlink);
+        println!("Access: ({:o}/{})  Uid: {}  Gid: {}", 
+            mode & 0o7777,
+            perm_string,
+            attr.uid,
+            attr.gid,
+        );
+        println!("Access: {}", atime);
+        println!("Modify: {}", mtime);
+        println!("Change: {}", ctime);
+
+        println!("Type: {}", ftype);
 
         Ok(())
     }
